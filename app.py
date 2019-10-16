ca5ccfc8783d853c2ee533fb879371c9c1bd48ee
ECotner <ecotner@atd-us.com>
2019-10-16 00:31:54 -0400

    added some more routes

--- app.py
+++ app.py
@@ -6,12 +6,43 @@ A personal website. All works displayed here are subject to the Creative Commons
 'Attribution-NonCommercial-ShareAlike 4.0 International' license.
 """
 
-import flask
+from flask import Flask, g, render_template, url_for
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
+nav_links = {
+    "index": "Home",
+    "datascience": "Data Science",
+    "physics": "Physics",
+    "bio": "Bio",
+    "resume": "Resume",
+}
+
+
+@app.before_request
+def first():
+    g.nav_links = nav_links
 
 
 @app.route("/")
-def hello_world():
-    return "Hello world!"
+def index():
+    return render_template("index.html")
 
+
+@app.route("/datascience")
+def datascience():
+    return render_template("index.html")
+
+
+@app.route("/physics")
+def physics():
+    return render_template("index.html")
+
+
+@app.route("/bio")
+def bio():
+    return f"This is my about page, {url_for('bio')}"
+
+
+@app.route("/resume")
+def resume():
+    return "Not yet!"
