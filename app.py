6279b229624db899e2751ab4e702c66333c0f18d
ECotner <ecotner@atd-us.com>
2019-10-20 22:42:52 -0400

    condensed the number of navbar links

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ A personal website. All works displayed here are subject to the Creative Commons
 'Attribution-NonCommercial-ShareAlike 4.0 International' license.
 """
 
+import flask
 from flask import Flask, g, render_template, url_for, redirect
 from flask_misaka import Misaka
 
@@ -14,13 +15,11 @@ Misaka(app=app, math_explicit=True, math=True, highlight=True, fenced_code=False
 
 nav_links = {
     "index": "Home",
-    # "physics": "Physics",
-    # "datascience": "Data Science",
-    # "machinelearning": "Machine Learning",
+    "physics": "Physics",
+    "datascience": "Data / AI",
     "github": "GitHub",
-    # "bio": "Bio",
-    # "resume": "Resume",
-    # "blog": "Blog",
+    "blog": "Blog",
+    "about": "About / Contact",
 }
 
 
@@ -41,14 +40,12 @@ def datascience():
     return index()
 
 
-@app.route("/machinelearning")
-def machinelearning():
-    return index()
-
-
 @app.route("/physics")
 def physics():
-    return index()
+    with app.open_resource("markdown/physics.md", mode="r") as fo:
+        text = fo.read()
+    tmp = render_template("physics.html", text=text)
+    return flask.render_template_string(tmp)
 
 
 @app.route("/github")
@@ -56,14 +53,9 @@ def github():
     return redirect(location=r"https://github.com/ecotner")
 
 
-@app.route("/bio")
-def bio():
-    return f"This is my about page, {url_for('bio')}"
-
-
-@app.route("/resume")
-def resume():
-    return "Not yet!"
+@app.route("/about")
+def about():
+    return f"This is my about page, {url_for('about')}"
 
 
 @app.route("/blog")
@@ -71,5 +63,5 @@ def blog():
     return index()
 
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
